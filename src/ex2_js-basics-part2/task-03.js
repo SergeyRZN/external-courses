4d8201467de1ce020982888759a78202ab0c7f30
SergeyRZN <vedutenkos@mail.ru>
2019-10-03 19:44:56 +0300

    Replaced switch with if. Removed the console call

--- src/ex2_js-basics-part2/task-03.js
+++ src/ex2_js-basics-part2/task-03.js
@@ -2,24 +2,20 @@
 function checkNumber(myArray) {
     let notEventNumber = 0, eventNumber = 0, zeroNumber = 0, arrayNumber;
     for (let i = 0; i < myArray.length; i++) {
-        if(typeof(myArray[i]) !== "number") {
+        if(typeof myArray[i] !== "number") {
           continue;
         };
         if(myArray[i] === 0) {
-          zeroNumber = zeroNumber + 1;
+          zeroNumber += 1;
           continue;
         };
-        switch (myArray[i] % 2) {
-        case 0:
-            eventNumber = eventNumber + 1;
-            break;
-        default:
-        notEventNumber = notEventNumber +1;
-        }
-    }
-    console.log (eventNumber);
-    console.log (notEventNumber);
-    console.log (zeroNumber);
+		if (myArray[i] % 2 === 0) {
+			eventNumber += 1;
+		} else {
+			notEventNumber += 1;
+			};
+        };
+    
     arrayNumber = [eventNumber,notEventNumber,zeroNumber];
     return arrayNumber;
 }
