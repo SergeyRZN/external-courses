280be37761c8d029cb5a3ccb244d5a86b0d110f0
SergeyRZN <vedutenkos@mail.ru>
2019-11-10 16:15:17 +0300

    edit calculator

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -4,30 +4,26 @@ let calculator = {
 	add(value = 0){ 
 		if(typeof value === "number") {
 			calculator.result += value;
-			return calculator.add;
 		};
-		return add;
+		return calculator.add;
 	},
     subtract(value = 0) {
 		if(typeof value === "number") {
 			calculator.result -= value;
-			return calculator.subtract;
 		};
-		return subtract;
+		return calculator.subtract;
     },
     divide(value = 1) {
 		if(typeof value === "number") {
 			calculator.result /= value;
-			return calculator.divide;
 		};
-		return divide;
+		return calculator.divide;
     },
     multiply(value = 1) {
 		if(typeof value === "number") {
 			calculator.result *= value;
-			return calculator.multiply;
 		};
-		return multiply;
+		return calculator.multiply;
     },   
     getResult() {
         return calculator.result;
