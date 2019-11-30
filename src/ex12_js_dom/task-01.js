9b3e381d7326650eb3c8d2fb4e48c022fae06219
SergeyRZN <vedutenkos@mail.ru>
2019-11-30 21:51:22 +0300

    fixed defects

--- src/ex12_js_dom/task-01.js
+++ src/ex12_js_dom/task-01.js
@@ -1,16 +1,31 @@
+const slides = document.getElementsByClassName("item");
+const arrayImage = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg'];
+const elementAdd = function (array) {
+	array.forEach(function(img) {
+		let elementsDivGeneral= document.getElementById('slider');
+		let elementDiv = document.createElement('div');
+		let elementImg = document.createElement('img');
+		elementDiv.setAttribute('class', 'item');
+		elementImg.setAttribute('src', img);
+		elementDiv.append(elementImg);
+		elementsDivGeneral.append(elementDiv);	
+	});
+};
 let slideIndex = 1;
+
+elementAdd(arrayImage)
+
 showSlides(slideIndex);
  
 function nextSlide() {
     showSlides(slideIndex += 1);
-}
+};
 
 function backSlide() {
     showSlides(slideIndex -= 1);  
-}
+};
  
 function showSlides(n) {
-    let slides = document.getElementsByClassName("item");
     if (n > slides.length) {
       slideIndex = 1;
     }
@@ -21,4 +36,4 @@ function showSlides(n) {
 		slides[i].style.display = "none";
     }
     slides[slideIndex - 1].style.display = "block";
-}
+};
